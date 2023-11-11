3330d5b5375ed0443ce731c1e7bc918c6ec66889
deityseven <3243881379@qq.com>
2023-11-11 18:14:34 +0800

    更新配置文件解析器

--- src/projects/test/main.cpp
+++ src/projects/test/main.cpp
@@ -1,15 +1,21 @@
 #include <config_parser/config_parser.h>
 
 #include <iostream>
+#include <map>
+#include <string>
+
+struct T
+{
+	std::string key;
+	int value;
+};
 
 int main(int argc, char *argv[])
 {
-	std::cout << ConfigParser::test();
+	ConfigParser cp("./configs/system.ini");
 
-	std::cout << "change..." << std::endl;
-
-
-	std::cout << "test..." << std::endl;
+	std::cout << cp.getValue("key2");
+	std::cout << cp.getValue("sysytem","ip");
 
 	return 0;
 }
